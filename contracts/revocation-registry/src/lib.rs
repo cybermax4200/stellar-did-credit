52933dd3440f3dfdea22488bea9708e854bf0844
AJ0070 <jashambaliya1@gmail.com>
2026-06-28 13:08:48 +0530

    fix

--- contracts/revocation-registry/src/lib.rs
+++ contracts/revocation-registry/src/lib.rs
@@ -262,12 +262,12 @@ mod tests {
         let vc_hash = BytesN::from_array(&env, &[3u8; 32]);
 
         // First revoke registers issuer_a for this vc_hash.
-        client.revoke(&issuer_a, &vc_hash).unwrap();
+        client.revoke(&issuer_a, &vc_hash);
         assert!(client.is_revoked(&vc_hash));
 
         // issuer_b must not be able to revoke the same hash.
-        let res = client.revoke(&issuer_b, &vc_hash);
-        assert_eq!(res, Err(RevocationRegistryError::IssuerMismatch));
+        let res = client.try_revoke(&issuer_b, &vc_hash);
+        assert_eq!(res, Err(Ok(RevocationRegistryError::IssuerMismatch)));
     }
 
     #[test]
@@ -310,9 +310,7 @@ mod tests {
         client.upgrade(&admin2, &BytesN::from_array(&env, &[0u8; 32]));
 
         // old admin cannot upgrade
-        let res = std::panic::catch_unwind(|| {
-            client.upgrade(&admin1, &BytesN::from_array(&env, &[1u8; 32]));
-        });
+        let res = client.try_upgrade(&admin1, &BytesN::from_array(&env, &[1u8; 32]));
         assert!(res.is_err());
     }
 
